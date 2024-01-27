d9c3d86856721174804dde9d464795ad740a8bff
Petrescu Alexandru <petrescu_robert94@yahoo.com>
2024-01-27 23:58:58 +0200

    update errors

--- src/components/ImageGallery/ImageGallery.styled.jsx
+++ src/components/ImageGallery/ImageGallery.styled.jsx
@@ -10,15 +10,13 @@ export const GalleryListStyled = styled.ul`
   padding: 0;
   list-style: none;
   margin-left: auto;
-  margin-right: auto;
-`;
-
+  margin-right: auto;`;
+  
 export const GalleryItemStyled = styled.li`
   border-radius: 2px;
   box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
-    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
-`;
-
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);`;
+    
 export const ImageStyled = styled.img`
   width: 100%;
   height: 260px;
@@ -28,8 +26,7 @@ export const ImageStyled = styled.img`
   &:hover {
     transform: scale(1.03);
     cursor: zoom-in;
-  }
-`;
+  }`;
 
 export const ImageLarge = styled.img`
   width: 700px;
@@ -40,5 +37,4 @@ export const ImageLarge = styled.img`
   &:hover {
     transform: scale(1.03);
     cursor: zoom-in;
-  }
-`;
+  }`;
